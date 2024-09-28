d9a550b895cbecdc3fbeab7929d856a416d3231d
Rádler Ákos <akos.radler@gmail.com>
2024-09-28 20:42:48 +0200

    App init

---
+++ app/routes/_.tsx
@@ -0,0 +1,25 @@
+import { Box, CssBaseline, ThemeProvider, useTheme } from "@mui/material";
+import { type MetaFunction } from "@remix-run/node";
+import { Outlet } from "@remix-run/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Devertix frontend" }, { name: "description", content: "Devertix frontend" }];
+};
+
+const queryClient = new QueryClient();
+
+export default function Index() {
+  const theme = useTheme();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box display={"flex"} alignItems={"center"} flexDirection={"column"} justifyContent={"center"}>
+          <Outlet />
+        </Box>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
