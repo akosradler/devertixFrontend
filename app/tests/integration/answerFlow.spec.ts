9c22a887ef92d7d8faadc0aaea81787ac366f7c7
Rádler Ákos <akos.radler@gmail.com>
2024-10-01 11:50:52 +0200

    Move mock out to its own file

--- app/tests/integration/answerFlow.spec.ts
+++ app/tests/integration/answerFlow.spec.ts
@@ -1,24 +1,8 @@
-describe("Answer flow", () => {
-  const mockData = [
-    {
-      id: "a246392e-89b0-44a6-9a13-308b6c8ed602",
-      question: "Are you handsome?",
-      answer: true,
-    },
-    {
-      id: "168043e4-1120-443a-8445-042f26d1e30a",
-      question: "Are you smart?",
-      answer: true,
-    },
-    {
-      id: "286b4899-89ec-4ce3-a7b7-431d7e438c76",
-      question: "Are you a dummy?",
-      answer: false,
-    },
-  ];
+import { questionIntercepterMockData } from "./mocks/questionIntercepterData";
 
+describe("Answer flow", () => {
   beforeEach(() => {
-    cy.intercept("GET", "/question", mockData);
+    cy.intercept("GET", "/question", questionIntercepterMockData);
     cy.visit("/");
   });
 
